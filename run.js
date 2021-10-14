bc6b60ee34af4d1ede768a5de86ce7ba4e27c79b
Martin <martinloesethjensen@gmail.com>
2021-10-15 00:31:52 +0200

    feat: added new commands for start-amount, limit, and flagging for whether it should decrease or increase

--- run.js
+++ run.js
@@ -2,6 +2,7 @@
 const { ApiPromise, WsProvider } = require("@polkadot/api");
 const { Keyring } = require("@polkadot/keyring");
 const fs = require("fs");
+const { resolve } = require("path");
 
 const options = require("yargs")
   .option("id", {
@@ -34,12 +35,28 @@ const options = require("yargs")
     type: "num",
     description: "Timeinterval for listing in seconds",
     required: true,
+  })
+  .option("start-amount", {
+    type: "num",
+    description: "The starting amount for listing",
+    required: false,
+  })
+  .option("limit", {
+    type: "num",
+    description: "The limit amount for listing",
+    required: false,
+  })
+  .option("decrease", {
+    type: "boolean",
+    description: "Flag for decreasing the listing",
+    required: false,
+    default: false,
   }).argv;
 
 let ROUND = 1;
 
 async function main() {
-  const provider = new WsProvider(options['endpoint']);
+  const provider = new WsProvider(options["endpoint"]);
 
   const api = await ApiPromise.create({ provider });
 
@@ -64,26 +81,69 @@ async function main() {
       : undefined;
   const amount = options["amount"];
   const time = options["timeinterval"];
+  const startAmount =
+    typeof options["start-amount"] !== "undefined"
+      ? options["start-amount"]
+      : 0;
+  const limit = options["limit"];
+  const shouldDecrease = options["decrease"];
 
   /// KSM Precision [kusama guide](https://guide.kusama.network/docs/kusama-parameters/#precision)
   const ksmPrecision = 1_000_000_000_000;
 
   const amountPrecision = amount * ksmPrecision;
+  const startAmountPrecision = startAmount * ksmPrecision;
+  const limitPrecision = limit * ksmPrecision;
 
+  console.log("💰 Starting amount:", startAmount);
   console.log("💰 Amount to be listed linearly:", amount);
 
   let account = keyring.addFromUri(key[0]);
   console.log("🤖 ACCOUNT_ADDRESS:", account.address);
 
+  const shouldPutStartingAmount = startAmount > 0 && ROUND == 1;
+
+  if (shouldPutStartingAmount) {
+    const tx = api.tx.system.remark(
+      `RMRK::LIST::1.0.0::${rmrkId}::${startAmountPrecision}`
+    );
+    console.log(tx.toHuman());
+
+    //const _ = await sendAndFinalize(tx, account);
+  }
+
   await new Promise((_) =>
     setInterval(async function () {
+
+      var _amount;
+
+      if (shouldDecrease) {
+        _amount = startAmountPrecision - amountPrecision * ROUND;
+      } else {
+        _amount = startAmountPrecision + amountPrecision * ROUND;
+      }
+
       const tx = api.tx.system.remark(
-        `RMRK::LIST::1.0.0::${rmrkId}::${amountPrecision * ROUND}`
+        `RMRK::LIST::1.0.0::${rmrkId}::${_amount}`
       );
       ROUND++;
       console.log(tx.toHuman());
 
-      await sendAndFinalize(tx, account);
+      if (shouldDecrease && _amount <= 0) {
+        throw new Error("Amount have reached 0");
+      } 
+
+      if (limit !== undefined) {
+        if (shouldDecrease && _amount < limitPrecision) {
+          // exit
+          process.exit();
+        } else if (!shouldDecrease && _amount > limitPrecision) {
+          // exit 
+          process.exit()
+        }
+      }
+
+      //const _ = await sendAndFinalize(tx, account);
     }, time * 1000)
   );
 }
